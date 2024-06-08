b73896c8c41c7d21e8804d567e3e35e100a23a30
qsnk <mikhailroot01@gmail.com>
2024-06-08 21:59:32 +0300

    Added filters to cameras, places, statistics templates. Added js & css files. Updated templates, views, models, settings, forms

--- safety_site/cabinet/models.py
+++ safety_site/cabinet/models.py
@@ -6,14 +6,16 @@ from django.contrib.auth.models import User
 class NeuralNetwork(models.Model):
     name = models.CharField(name="name", max_length=100, null=False, help_text="Введите название нейронной сети", verbose_name="Название нейронной сети")
     description = models.CharField(name="description", max_length=200, null=True, help_text="Введите описание нейронной сети", verbose_name="Описание нейронной сети")
+    date_time = models.DateTimeField(name="date_time", auto_now_add=True, verbose_name="Дата и время добавления", null=False, help_text="Введите дату и время добавления")
     file = models.FileField(name="file", verbose_name="Файл", upload_to="neural_networks/")
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
 
 
 class Camera(models.Model):
     name = models.CharField(name="name",max_length=100, null=False, help_text="Введите название камеры", verbose_name="Название камеры")
+    date_time = models.DateTimeField(name="date_time", auto_now_add=True, verbose_name="Дата и время добавления", null=False, help_text="Введите дату и время добавления")
     url = models.CharField(name="url", max_length=300, null=False, help_text="Введите ссылку для подключения к камере", verbose_name="Ссылка для подключения к камере")
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id", verbose_name="Идентификатор пользователя")
 
@@ -23,7 +25,8 @@ class Camera(models.Model):
 
 class Place(models.Model):
     name = models.CharField(name="name", max_length=100, null=False, help_text="Введите название площадки", verbose_name="Название площадки")
-    description = models.CharField(name="description", max_length=200, null=True, help_text="Введите описание площадки", verbose_name="Описание площадки")
+    description = models.CharField(name="description", max_length=200, null=True, default="Описания нет", help_text="Введите описание площадки", verbose_name="Описание площадки")
+    date_time = models.DateTimeField(name="date_time", auto_now_add=True, verbose_name="Дата и время добавления", null=False, help_text="Введите дату и время добавления")
     camera_id = models.ForeignKey(Camera, on_delete=models.CASCADE, db_column="camera_id", verbose_name="Идентификатор камеры", default=None)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id", verbose_name="Идентификатор пользователя", default=None)
 
@@ -40,6 +43,7 @@ class Violation(models.Model):
 
     def __str__(self):
         return f'{self.violation_class} [{self.date_time.strftime("%d-%m-%Y %H:%M")}]'
+
 
 class Report(models.Model):
     name = models.CharField(name="name", max_length=200, null=False, help_text="Введите название отчета", verbose_name="Название отчета")
